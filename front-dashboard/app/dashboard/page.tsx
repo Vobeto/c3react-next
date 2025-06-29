d828f1d414d88140d7d19df1174f762ee91c6a26
Pedro Armando Vobeto <136371056+Vobeto@users.noreply.github.com>
2025-06-28 23:40:38 -0300

    Add files via upload

--- front-dashboard/app/dashboard/page.tsx
+++ front-dashboard/app/dashboard/page.tsx
@@ -1,6 +1,7 @@
+// app/page.tsx
 'use client';
 import Dashboard from '@/components/dashboard';
 
-export default function DashboardPage() {
+export default function Home() {
   return <Dashboard />;
 }
